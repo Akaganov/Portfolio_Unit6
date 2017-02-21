fba6db6f78bb3c81f4ace63b85d4e90d3ed168be
Anna Kaganov <anna_k_a@icloud.com>
2017-02-21 01:00:39 +0000

    started project

---
+++ HarryPotter.js
@@ -0,0 +1,113 @@
+$("#2").click(function(){
+  
+ var multiple = "#a1";
+if
+(multiple === "Albus Percival Wulfric Brian Dumbledore")
+{$("#a").html("correct");
+$("#a").css("color","green");
+  }   
+    else
+    {$("#a").html("wrong");
+$("#a").css("color","red");
+    }
+ var multiple = "#b1";
+ if
+(multiple === "Rabbit")
+{$("#b").html("correct");
+$("#b").css("color","green");
+}
+else
+{$("#b").html("wrong");
+$("#b").css("color","red");
+}
+ var multiple = "#c1";
+ if
+(multiple === "4 Privet Drive in Little Whinging, Surrey,in The Cupboard")
+{
+ $("#c").html("correct");
+$("#c").css("color","green");
+}
+else
+{$("#c").html("wrong");
+$("#c").css("color","red");
+}
+ var multiple = "#d1";
+ if
+(multiple === "Blue")
+{$("#d").html("correct");
+$("#d").css("color","green");
+}
+
+else
+{$("#d").html("wrong");
+$("#d").css("color","red");
+}
+ var multiple = "#e1";
+ if
+(multiple === "8 ft 6 in")
+{$("#e").html("correct");
+$("#e").css("color","green");
+}
+else
+{$("#e").html("wrong");
+$("#e").css("color","red");
+}
+ var multiple = "#f1";
+ if
+(multiple === "Augusta Longbottom")
+{$("#f").html("correct");
+$("#f").css("color","green");
+}
+else
+{$("#f").html("wrong");
+$("#f").css("color","red");
+}
+var multiple = "#g1";
+ if
+(multiple === "Rolanda Hooch")
+{$("#g").html("correct");
+$("#g").css("color","green");
+}
+else
+{$("#g").html("wrong");
+$("#g").css("color","red");
+}
+var multiple = "#h1";
+ if
+(multiple === "Swedish Short-snout")
+{$("#h").html("correct");
+$("#h").css("color","green");
+}
+else
+{$("#h").html("wrong");
+$("#h").css("color","red");
+}
+var multiple = "#h1";
+ if
+(multiple === "150 mph")
+{$("#i").html("correct");
+$("#i").css("color","green");
+}
+else
+{$("#i").html("wrong");
+$("#i").css("color","red");
+}
+var multiple = "#i1";
+
+ var multiple = "#j1";
+ if
+(multiple === "665")
+{$("#j").html("correct");
+$("#j").css("color","green");
+}
+else
+{$("#j").html("wrong");
+$("#j").css("color","red");
+}
+});
+$("#1").click(function(){
+    location.reload();
+});
+
+
+
