7d7400d70a29a3be3ce380a07151e40a021d81ff
Anna Kaganov <anna_k_a@icloud.com>
2017-04-01 02:38:21 +0000

    Fixed hary potter

--- QUIZES/ANIMALQUIZ/AnimalQuiz.js
+++ QUIZES/ANIMALQUIZ/AnimalQuiz.js
@@ -1,4 +1,43 @@
-/* global $ */
- $( "#Results" ).click(function() {
-  alert( "Handler for .click() called." );
- });
+/*global $ */
+$("#2").click(function(){
+ var multiple = $("#a1").val();
+     if (multiple == "answer 1") {
+       $("#a").html("right");
+       $("#a").css("color","green");
+     } else {
+       $("#a").html("wrong");
+       $("#a").css("color","red");
+     }
+ var multiple = $("#b1").val();
+     if (multiple == "answer 2") {
+       $("#b").html("right");
+       $("#b").css("color","green");
+     } else {
+       $("#b").html("wrong");
+       $("#b").css("color","red");
+     }
+     var multiple = $("#c1").val();
+     if (multiple == "answer 3") {
+       $("#c").html("right");
+       $("#c").css("color","green");
+     } else {
+       $("#c").html("wrong");
+       $("#c").css("color","red");
+     }
+      var multiple = $("#d1").val();
+     if (multiple == "answer 4") {
+       $("#d").html("right");
+       $("#d").css("color","green");
+     } else {
+       $("#d").html("wrong");
+       $("#d").css("color","red");
+     }
+      var multiple = $("#e1").val();
+     if (multiple == "answer 5") {
+       $("#e").html("right");
+       $("#e").css("color","green");
+     } else {
+       $("#e").html("wrong");
+       $("#e").css("color","red");
+     }
+     });
